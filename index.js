894161d7b7a293d6d4c1dc1fcc6958135fae1218
yangjie <yangjie@yangjiedeMacBook-Pro.local>
2017-12-03 14:43:08 +0800

    init

---
+++ index.js
@@ -0,0 +1,20 @@
+'use strict';
+var through2 = require('through2');
+
+module.exports = function(options) {
+  return through2.obj( function ( file, enc, callback ) {
+    var rootPX = ( options && options.rootPX ) || 75;
+    if( file.isNull() ) {
+      return callback( null, file );
+    }
+    if( file.isStream() ) {
+      return callback(new PluginError('gulp-px-to-rem', 'Streaming not supported'));
+    }
+    var content = file.contents.toString();
+    var str = content.replace(/((-?\d+)(\.\d+)?)px/ig, function($0, $1) {
+      return ($1 / rootPX) + 'rem'
+    });
+    file.contents = new Buffer(str);
+    return callback(null, file);
+  })
+}
